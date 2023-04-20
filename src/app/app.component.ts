977342cf9f5e31189d7ab35d3ea103cf22a9feeb
talinpk <96902115+talinpk@users.noreply.github.com>
2023-04-20 14:50:44 -0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angluar2';
+  users = [
+    {id: 1, name: 'John Doe', email: 'john@example.com'},
+    {id: 2, name: 'Jane Doe', email: 'jane@example.com'},
+    {id: 3, name: 'Bob Smith', email: 'bob@example.com'}
+  ];
+  constructor() { }
+
+  addUser(user) {
+    user.id = this.users.length + 1;
+    this.users.push(user);
+  }
+
+  getUsers() {
+    return this.users;
+  }
+
+  updateUser(user) {
+    const index = this.users.findIndex(u => u.id === user.id);
+    this.users[index] = user;
+  }
+
+  deleteUser(id) {
+    const index = this.users.findIndex(u => u.id === id);
+    this.users.splice(index, 1);
+  }
 }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
